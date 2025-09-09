4f52da9f2295bffd85cdc9d7af02d91e278a36d6
hugomonntes <hmontes.dev@gmail.com>
2025-09-09 10:56:25 +0200

    Update

---
+++ T1/Hola/ejemplo1.java
@@ -0,0 +1,13 @@
+
+import java.io.File;
+
+public class ejemplo1 {
+    public static void main(String[] args) {
+        File newFile = new File("Hola");
+        System.out.println(newFile.getName());
+        System.out.println(newFile.getPath());
+        System.out.println(newFile.exists());
+        newFile.mkdir();
+        newFile.renameTo(new File("Holaaaa"));
+    }
+}
