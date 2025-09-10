8426b5b219f09a5b1ab00276ef7a11b80fac7c5f
hugomonntes <hmontes.dev@gmail.com>
2025-09-10 12:33:39 +0200

    Update

--- T1/Hola/ejemplo1.java
+++ T1/Hola/ejemplo1.java
@@ -2,12 +2,17 @@
 import java.io.File;
 
 public class ejemplo1 {
+
     public static void main(String[] args) {
         File newFile = new File("Hola");
-        System.out.println(newFile.getName());
-        System.out.println(newFile.getPath());
-        System.out.println(newFile.exists());
-        newFile.mkdir();
-        newFile.renameTo(new File("Holaaaa"));
+        // System.out.println(newFile.getName());
+        // System.out.println(newFile.getPath());
+        // System.out.println(newFile.exists());
+        // newFile.mkdir();
+        // newFile.renameTo(new File("Holaaaa"));
+        File[] arrayArchivos = newFile.listFiles();
+        for (int i = 0; i < arrayArchivos.length; i++) {
+            System.out.println(arrayArchivos[i].getName());
+        }
     }
 }
---
+++ T1/ListarArchivos.java
@@ -0,0 +1,14 @@
+
+import java.io.File;
+
+public class ListarArchivos {
+
+    public static void main(String[] args) {
+        File tema = new File("C:\\Users\\Hugo Montes\\Documents\\DataAccess\\T1");
+        for (File doc : tema.listFiles()) {
+            if (doc.isDirectory()) {
+                System.out.println(doc.getName() + " es un directorio");
+            }
+        }
+    }
+}
