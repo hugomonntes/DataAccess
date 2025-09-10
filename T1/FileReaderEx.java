294da9ab737df92471dd70a1d677a4aee910c302
hugomonntes <hmontes.dev@gmail.com>
2025-09-10 13:15:29 +0200

    Update

--- T1/FileReaderEx.java
+++ T1/FileReaderEx.java
@@ -11,6 +11,19 @@ public class FileReaderEx {
             while ((i = fr.read()) != -1) {
                 System.out.print((char) i);
             }
+            System.out.println();
+        }
+
+        try (FileReader fr = new FileReader("T1\\Teoría.md")) {
+            char[] buffer = new char[4];
+            int i = 0;
+            for (int j = 0; j < 0 || (i = fr.read()) != -1; j++) {
+                buffer[j] = (char) i;
+            }
+
+            for (char c : buffer) {
+                System.out.print(c);
+            }
         }
     }
 }
