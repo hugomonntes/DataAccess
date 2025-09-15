fd6f59586e1cf3b0abc73a2097cd7d0d43105304
hugomonntes <hmontes.dev@gmail.com>
2025-09-15 17:01:13 +0200

    Update

--- T1/Ex2.java
+++ T1/Ex2.java
@@ -19,13 +19,32 @@ public class Ex2 {
         }
         return String.format("Numero veces: %d, Lineas: %s", counterLines, numberOfLineIsFind);
     }
+
+    static String searchLineAumented(String fileName, String wordToCompare){
+        int counterLines = 0;
+        String numberOfLineIsFind = "";
+        try (Scanner sc = new Scanner(new File(fileName))) {
+            while (sc.hasNext()) {
+                String[] textToCompare = sc.nextLine().split(" ");
+                for (String string : textToCompare) {
+                    if(string.equals(wordToCompare)){
+                        counterLines++;
+                        numberOfLineIsFind += counterLines + " ";
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.getMessage();
+        }
+        return String.format("Numero veces: %d, Lineas: %s", counterLines, numberOfLineIsFind);
+    }
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Introduce el nombre del archivo con su extensión: ");
             String fileName = sc.nextLine();
             System.out.print("Introduce la palabra a buscar en el archivo: ");
             String wordToCompare = sc.nextLine();
-            System.out.println(searchLine(fileName,wordToCompare));
+            System.out.println(searchLineAumented(fileName,wordToCompare));
         }
     }
 }
