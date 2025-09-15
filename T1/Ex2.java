b9ba4d34a2094b1a7426d2cc6e740349e4ae6f95
hugomonntes <hmontes.dev@gmail.com>
2025-09-15 17:00:58 +0200

    Update

--- T1/Ex2.java
+++ T1/Ex2.java
@@ -1,23 +1,31 @@
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Ex2 {
-    static String searchLine(String wordToCompare){
+    static String searchLine(String fileName, String wordToCompare){
         int counterLines = 0;
         String numberOfLineIsFind = "";
-        try (Scanner sc = new Scanner(new File("archivo.txt"))) {
+        try (Scanner sc = new Scanner(new File(fileName))) {
             while (sc.hasNext()) { 
                 if(sc.nextLine().contains(wordToCompare)){
                     counterLines++;
                     numberOfLineIsFind += counterLines + " ";
                 }
             }
-        } catch (Exception e) {
+        } catch (FileNotFoundException e) {
+            e.getMessage();
         }
         return String.format("Numero veces: %d, Lineas: %s", counterLines, numberOfLineIsFind);
     }
     public static void main(String[] args) {
-        System.out.println(searchLine("Celta"));
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Introduce el nombre del archivo con su extensión: ");
+            String fileName = sc.nextLine();
+            System.out.print("Introduce la palabra a buscar en el archivo: ");
+            String wordToCompare = sc.nextLine();
+            System.out.println(searchLine(fileName,wordToCompare));
+        }
     }
 }
