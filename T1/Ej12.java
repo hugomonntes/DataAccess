d03c68989f00063cf9dea6e4b37e3fde25315ec4
hugomonntes <hmontes.dev@gmail.com>
2025-09-18 12:59:22 +0200

    Update

--- T1/Ej12.java
+++ T1/Ej12.java
@@ -1,10 +1,28 @@
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class Ej12 {
     public static void escribirObjetos(File fichero, ArrayList<Cliente> clientes){
-        
+        FileOutputStream fos = null;
+        ObjectOutputStream out = null;
+        try {
+            fos = new FileOutputStream(fichero);
+            if (fichero.length() == 0) {
+                out = new ObjectOutputStream(fos);
+            } else {
+                // out = new 
+            }
+
+            for (Cliente cliente : clientes) {
+                out.writeObject(cliente);
+            }
+            fos.close();
+            out.close();
+        } catch (Exception e) {
+        }
     }
 
     public static void main(String[] args) {
@@ -15,6 +33,5 @@ public class Ej12 {
         personas = new ArrayList<>();
         personas.add(new Persona("Lugonpa", 21));
         File fichero = new File("clientes.dat");
-        
     }
 }
