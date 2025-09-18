28a44a2c1b79c9d6ff9e721080c0addacee2e31a
hugomonntes <hmontes.dev@gmail.com>
2025-09-18 13:38:50 +0200

    Update

---
+++ T1EX/AD_EX4.java
@@ -0,0 +1,30 @@
+package T1EX;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AD_EX4 {
+    public static void calcularCharMasRepetido(ArrayList<Character> chars){
+        for (Character character : chars) {
+            System.out.println(character);
+        }
+    }
+
+    public static ArrayList<Character> filtroLeerArchivo(String nombreLeerArchivo){
+        ArrayList<Character> charDivididas = new ArrayList<>();
+        try (Scanner sc = new Scanner(new File(nombreLeerArchivo))) {
+            while (sc.hasNext()) {
+                String textoEntero = sc.nextLine();
+                for (int i = 0; i < textoEntero.length(); i++) {
+                    charDivididas.add(textoEntero.charAt(i));
+                }
+            }
+        } catch (Exception e) {
+        }
+        return charDivididas;
+    }
+    public static void main(String[] args) {
+        calcularCharMasRepetido(filtroLeerArchivo("a.txt"));
+    }
+}
