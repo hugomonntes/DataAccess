84e36d96b49efd83be2eba2315c04291fad28636
hmontes <hmontes.dev@gmail.com>
2025-10-01 13:03:33 +0200

    Update

--- data_access/src/main/java/data_access/T2/dom.java
+++ data_access/src/main/java/data_access/T2/dom.java
@@ -1,3 +1,28 @@
+/*-
+ * =====LICENSE-START=====
+ * Java 11 Application
+ * ------
+ * Copyright (C) 2020 - 2025 Organization Name
+ * ------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * =====LICENSE-END=====
+ */
 package data_access.T2;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -13,6 +38,7 @@ public class dom {
             factoria.setIgnoringComments(true);
             DocumentBuilder builder = factoria.newDocumentBuilder();
             doc = builder.parse(ruta);
+            System.out.println("Arbol DOM creado");
         } catch (Exception e) {
             System.out.println("Error generando el árbol DOM: " + e.getMessage());
         }
