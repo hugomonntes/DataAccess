2e596fdc884d2c4a4f64ae8d704a6fc4c0d660e6
hmontes <hmontes.dev@gmail.com>
2025-10-20 18:20:34 +0200

    Add funcionality startElement ex15

--- data_access/src/main/java/data_access/SAX/Ex_sax15.java
+++ data_access/src/main/java/data_access/SAX/Ex_sax15.java
@@ -18,28 +18,20 @@ public class Ex_sax15 extends DefaultHandler {
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
         super.characters(ch, start, length);
-        System.out.print(new String(ch, start, length));
+        // if(new String(ch,start,length).equals("pelicula")){
+        //     System.out.println("a");
+        // }
+        // System.out.print(new String(ch,start,length));
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
         super.endElement(uri, localName, qName);
-        System.out.print("</" + qName + ">");
-
     }
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
         super.startElement(uri, localName, qName, attributes);
-        if (qName.equals("pelicula")) {
-            for (int i = 0; i < attributes.getLength(); i++) {
-                if(attributes.getLocalName(i).equals("genero")){
-                    System.out.print("<" + qName + " " + attributes.getLocalName(i) + "=\"" + attributes.getValue(i) + "\">");
-                }
-            }
-        } else {
-            System.out.print("<" + qName + ">");
-        }
     }
 }
 
