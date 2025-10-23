6881e81cf4b2d6ddcf396fcc7e8c1581e68c571b
hmontes <hmontes.dev@gmail.com>
2025-10-23 13:47:54 +0200

    Update

--- data_access/src/main/java/data_access/JSON/JsonUtils.java
+++ data_access/src/main/java/data_access/JSON/JsonUtils.java
@@ -125,6 +125,42 @@ public class JsonUtils {
         }
     }
 
+    public JsonArray creaArray() {
+        JsonArray array = (JsonArray) Json.createArrayBuilder()
+                .add(Json.createObjectBuilder()
+                        .add("titulo", "El atlas de las nubes")
+                        .add("año", 2012)
+                        .add("directores", "Lana Wachowski, Tom Tykwer, Lilly Wachowski")
+                        .add("interpretes", Json.createArrayBuilder()
+                                .add(Json.createObjectBuilder()
+                                        .add("nombre", "Tom Hanks")
+                                        .add("fechaNacimiento", Json.createObjectBuilder()
+                                                .add("año", "1956")
+                                                .add("mes", 8)))
+                                .add(Json.createObjectBuilder()
+                                        .add("nombre", "Halle Berry")
+                                        .add("fechaNacimiento", Json.createObjectBuilder()
+                                                .add("año", "1966")
+                                                .add("mes", 7)))))
+                .add(Json.createObjectBuilder()
+                        .add("titulo", "La red social")
+                        .add("año", 2010)
+                        .add("directores", "David Fincher")
+                        .add("interpretes", Json.createArrayBuilder()
+                                .add(Json.createObjectBuilder()
+                                        .add("nombre", "Jesse Eisenberg")
+                                        .add("fechaNacimiento", Json.createObjectBuilder()
+                                                .add("año", "1983")
+                                                .add("mes", 9)))
+                                .add(Json.createObjectBuilder()
+                                        .add("nombre", "Andrew Garfield")
+                                        .add("fechaNacimiento", Json.createObjectBuilder()
+                                                .add("año", "1983")
+                                                .add("mes", 7)))))
+                .build();
+        return array;
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         // JsonValue jsonValue = leeJSON("https://pokeapi.co/api/v2/pokemon/ditto");
         // escribeJSON(jsonValue, new
