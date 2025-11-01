d823d88f37ba56feba47e81073761c4cfe143e5f
hugomonntes <huugomontees@gmail.com>
2025-11-01 13:08:38 +0100

    Create Ex11, fix 10

--- data_access/src/main/java/data_access/JSON/Json_Ex/Ex10.java
+++ data_access/src/main/java/data_access/JSON/Json_Ex/Ex10.java
@@ -1,32 +1,34 @@
 package data_access.JSON.Json_Ex;
 
+import java.util.ArrayList;
+
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonValue;
 
 public class Ex10 {
-    private static String API_URL_SPORTS = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=sports&countryCode=ES&apikey=AMXR5Rf8zlr7oGucsebGKvDCLOQmGUGE";
-    
-    public static void showAllEventsFromCountry(String API_ROOT){
-        if (!API_ROOT.isEmpty() || API_ROOT != null) {
-            JsonValue jsonResponse = JsonUtils.leeJSON(API_ROOT);
-            JsonObject rootObject = jsonResponse.asJsonObject();
-            JsonObject _embeddedObject = rootObject.getJsonObject("_embedded");
-            JsonArray eventsArray = _embeddedObject.getJsonArray("events");
-            for (JsonValue event : eventsArray) {
-                JsonObject eventObject = event.asJsonObject();
-                String eventName = eventObject.getString("name");
-                System.out.println(eventName);
-            }
-        }
+    public static String API_URL_SPORTS = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=sports&countryCode=ES&apikey=AMXR5Rf8zlr7oGucsebGKvDCLOQmGUGE";
+
+    public static JsonArray showAllEventsFromCountry(String API_ROOT) {
+        JsonValue jsonResponse = JsonUtils.leeJSON(API_ROOT);
+        JsonObject rootObject = jsonResponse.asJsonObject();
+        JsonObject _embeddedObject = rootObject.getJsonObject("_embedded");
+        JsonArray eventsArray = _embeddedObject.getJsonArray("events");
+        return eventsArray;
     }
-    
+
     public static void main(String[] args) {
-        showAllEventsFromCountry(API_URL_SPORTS);
+        JsonArray eventsArray = showAllEventsFromCountry(API_URL_SPORTS);
+        for (JsonValue event : eventsArray) {
+            JsonObject eventObject = event.asJsonObject();
+            String eventName = eventObject.getString("name");
+            System.out.println(eventName);
+        }
     }
 }
 
 // 10. Crea un método que dado un tipo nos muestre todos los eventos de un país
 // en concreto. Usa para ello Ticketmaster.
 
-// API_ROOT https://app.ticketmaster.com/discovery/v2/events.json?classificationName="+tipo_evento+"&countryCode=ES&apikey=AMXR5Rf8zlr7oGucsebGKvDCLOQmGUGE
+// API_ROOT
+// https://app.ticketmaster.com/discovery/v2/events.json?classificationName="+tipo_evento+"&countryCode=ES&apikey=AMXR5Rf8zlr7oGucsebGKvDCLOQmGUGE
---
+++ data_access/src/main/java/data_access/JSON/Json_Ex/Ex11.java
@@ -0,0 +1,33 @@
+package data_access.JSON.Json_Ex;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonValue;
+
+public class Ex11 {
+    public static void showPlaceInfo(JsonArray eventsArray) {
+        for (JsonValue event : eventsArray) {
+            JsonObject eventObjs = event.asJsonObject();
+            String eventName = eventObjs.getString("name");
+            JsonObject _embedded = eventObjs.getJsonObject("_embedded");
+            JsonArray venues = _embedded.getJsonArray("venues");
+            for (JsonValue venue : venues) {
+                JsonObject venueObj = venue.asJsonObject();
+                String locationName = venueObj.getString("name");
+                JsonObject cityObj = venueObj.getJsonObject("city");
+                String cityName = cityObj.getString("name");
+                System.out.println(eventName + " - " + locationName + " - " + cityName);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        showPlaceInfo(Ex10.showAllEventsFromCountry(Ex10.API_URL_SPORTS));
+    }
+}
+
+// 11. Crea dos métodos que, para cada evento anterior, muestre la información
+// detallada de cada lugar en el que se desarrolle y la información detallada
+// del
+// evento.
