dd5b1b077400855a6e7a8ccffe8e93dd990cdf2e
hugomonntes <huugomontees@gmail.com>
2025-11-01 14:24:11 +0100

    Create Ex11, fix 10

---
+++ data_access/src/main/java/data_access/JSON/Json_Ex/Ex12.java
@@ -0,0 +1,10 @@
+package data_access.JSON.Json_Ex;
+
+public class Ex12 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+// 12. ¿Cuál es el tiempo actual de cada ciudad en donde se desarrollen los
+// eventos encontrados?
