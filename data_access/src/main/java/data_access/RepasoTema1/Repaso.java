9ca40f257ba21099df4fdf7fcb8b05b6fb58d1b6
hmontes <hmontes.dev@gmail.com>
2025-11-10 18:15:42 +0100

    Update

--- data_access/src/main/java/data_access/RepasoTema1/Repaso.java
+++ data_access/src/main/java/data_access/RepasoTema1/Repaso.java
@@ -2,9 +2,9 @@ package data_access.RepasoTema1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Repaso {
     // 1. Crea un método que reciba una carpeta y liste el contenido de dicha
@@ -33,20 +33,51 @@ public class Repaso {
     // 3. Crea un método que permita buscar palabras en un fichero de texto. Se debe
     // mostrar el
     // número de ocurrencias de dicha palabra. Utiliza un buffer para la lectura.
-    public static void searchWords(String pathFile, String wordToSearch) throws FileNotFoundException{
-        try (FileReader fr = new FileReader(pathFile)) {
-            int i;
-            while ((i = fr.read()) != -1) { 
-                System.out.println((char)i);
+    public static void searchWords(String pathFile, String wordToSearch) throws FileNotFoundException {
+        int counter = 0;
+        try (Scanner sc = new Scanner(new File(pathFile))) {
+            String[] buffer;
+            while (sc.hasNextLine()) {
+                buffer = sc.nextLine().split(" ");
+                for (String word : buffer) {
+                    if (word.equals(wordToSearch)) {
+                        counter++;
+                    }
+                }
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
+        }
+        System.out.println("La palabra (" + wordToSearch + ") aparece " + counter + " veces");
+    }
+
+    // 4. Crea un método que permita eliminar todas las ocurrencias de una palabra
+    // dada en un
+    // fichero de texto. Este código producirá automáticamente un fichero con la
+    // siguiente
+    // nomenclatura: Si el fichero de entrada se llama fichero.txt, el fichero
+    // generado se
+    // llamará fichero_2.txt.
+    // Pista: nextLine.replaceAll(Lo que quieres borrar, Lo que quiere agregar)
+    public static void removeWordFromFile(String pathFile, String wordToRemove){
+        try (Scanner sc = new Scanner(new File(pathFile))) {
+            String[] buffer;
+            while (sc.hasNextLine()) {
+                buffer = sc.nextLine().split(" ");
+                for (String word : buffer) {
+                    if (word.equals(wordToRemove)) {
+                        
+                    }
+                }
+            }
+        } catch (Exception e) {
         }
     }
 
 
     public static void main(String[] args) {
         try {
-            //createFiles(5, "archivo", "data_access\\src\\main\\java\\data_access\\RepasoTema1\\Repaso.java");
+            // createFiles(5, "archivo",
+            // "data_access\\src\\main\\java\\data_access\\RepasoTema1\\Repaso.java");
             searchWords("archivo0.txt", "el");
         } catch (IOException ex) {
         }
