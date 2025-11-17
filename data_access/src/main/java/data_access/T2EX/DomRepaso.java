7c2e47e5275ce68009d34eda306d11330056cb9e
hugomonntes <huugomontees@gmail.com>
2025-11-17 20:21:15 +0100

    AD_EX4 Check

---
+++ data_access/src/main/java/data_access/T2EX/DomRepaso.java
@@ -0,0 +1,51 @@
+package data_access.T2EX;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class DomRepaso {
+    public static Document creaArbol(String ruta) {
+        Document doc = null;
+        try {
+            DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
+            factoria.setIgnoringComments(true);
+            DocumentBuilder builder = factoria.newDocumentBuilder();
+            doc = builder.parse(ruta);
+        } catch (Exception e) {
+            System.out.println("Error generando el árbol DOM: " + e.getMessage());
+        }
+        return doc;
+    }
+
+    // Crea un método que mostre os títulos das películas.
+    public static void mostrarTitulos(Document document) {
+        NodeList titulos = document.getElementsByTagName("titulo");
+        for (int i = 0; i < titulos.getLength(); i++) {
+            System.out.println(titulos.item(i).getTextContent());
+        }
+    }
+
+    // Crea un método que mostre todas as películas xunto co nome e apelido do
+    // seu/os
+    // seus directores ademais do xénero ao que pertence.
+    public static void mostrarPeliculasConDirector(Document doc){
+        NodeList titulos = doc.getElementsByTagName("titulo");
+        for (int i = 0; i < titulos.getLength(); i++) {
+            NodeList director = doc.getElementsByTagName("director");
+            for (int j = 0; j < director.item(i).getChildNodes().getLength(); j++) {
+                Node nombre = director.item(i).getChildNodes().item(j);
+                String titulo = titulos.item(j).getTextContent();
+                System.out.println(String.format("%s - %s",titulo,nombre.getTextContent()));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // mostrarTitulos(creaArbol("peliculas.xml"));
+        mostrarPeliculasConDirector(creaArbol("peliculas.xml"));
+    }
+}
